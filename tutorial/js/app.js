1a3f1eaf90b1fd31fa4f9150186c1a234745ee5e
Nicholas Polsin <n.polsin@comcast.net>
2015-11-24 16:20:59 -0800

    completes tutorial

--- tutorial/js/app.js
+++ tutorial/js/app.js
@@ -4,3 +4,5 @@ var smallData = [20, 40, 60, 80, 100];
 var largeRandomData = [80, 100, 50, 20, 90, 40, 100, 110, 40, 90, 40];
 
 //Your code goes here//
+//d3.select('body').append('h1').text('hello world');
+d3.select('body').selectAll('div').data(largeRandomData).enter().append('div').attr('class', 'bar').style('height', function(d){return d + 'px'});
